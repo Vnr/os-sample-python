86caadb2dfafeee45e5b75c64d17bb1546e28085
unknown <C:\Users\Sergey\AppData\Roaming\The Bat!>
2018-03-22 20:15:28 +0300

    requests test

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,17 @@
 from flask import Flask
+import requests
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
     return "Hello World!"
+
+@app.route('/requeststest')
+def requeststest():
+    #logging.info('*******import requests*************')
+    return requests.get("http://httpbin.org/get",
+    					headers={'Referer': 'https://pamyat-naroda.ru/', 'User-Agent': 'Mozilla'}).content  # verify=False not working
+
 
 if __name__ == "__main__":
     application.run()
