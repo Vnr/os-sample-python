15c6118d8871e50a0b101c9eb438ef654b1d8c3e
unknown <C:\Users\Sergey\AppData\Roaming\The Bat!>
2018-06-03 05:59:31 +0300

    test

--- wsgi.py
+++ wsgi.py
@@ -11,8 +11,8 @@ def hello():
 def requeststest():
     #logging.info('*******import requests*************')
     result = requests.post("http://httpbin.org/post",
-    					data={'great job': 'super'},
-    					headers={'Referer': 'https://pamyat-naroda.ru/', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko'})  # verify=False not working
+                        data={'great job': 'super'},
+                        headers={'Referer': 'https://pamyat-naroda.ru/', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko'})  # verify=False not working
     return Response(result.content, headers={'Access-Control-Allow-Origin': '*'})
 
 
@@ -32,15 +32,15 @@ def proxy(path):
     
     #https://cloud.google.com/appengine/docs/standard/python/issue-requests
     try:
-	    result = requests.post(url=url,
-	        data=request.data,
-	        headers={'Referer': 'https://pamyat-naroda.ru/', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0'})
-	except Exception as e:
-		return Response(e.message)
+        result = requests.post(url=url,
+            data=request.data,
+            headers={'Referer': 'https://pamyat-naroda.ru/', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0'})
+except Exception as e:
+        return Response(e.message)
 #    if result.status_code == 200:
 #      doSomethingWithResult(result.content)
     return Response(result.content, headers={'Access-Control-Allow-Origin': '*'})
 
 if __name__ == "__main__":
     application.run()
-	#app.run(host='0.0.0.0', port=8080)
+    #app.run(host='0.0.0.0', port=8080)
